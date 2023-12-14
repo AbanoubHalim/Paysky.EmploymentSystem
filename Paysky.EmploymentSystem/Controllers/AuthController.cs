7ac2f3b3c5d3c0201da1eee4bd9c7ef95bba12d8
abanoub.halim <abanoub.halim@redbullmobile.sa>
2023-12-14 11:29:29 +0200

    add authentication controller

--- Paysky.EmploymentSystem/Controllers/AuthController.cs
+++ Paysky.EmploymentSystem/Controllers/AuthController.cs
@@ -1,5 +1,7 @@
 ﻿using Microsoft.AspNetCore.Http;
 using Microsoft.AspNetCore.Mvc;
+using Paysky.APIServices.Contract;
+using Paysky.Entities.Models.AppModels;
 
 namespace Paysky.EmploymentSystem.Controllers
 {
@@ -7,5 +9,39 @@ namespace Paysky.EmploymentSystem.Controllers
 	[ApiController]
 	public class AuthController : ControllerBase
 	{
-	}
+        private readonly IAuthService _authService;
+
+        public AuthController(IAuthService authService)
+        {
+            _authService = authService;
+        }
+        [HttpPost("register")]
+        public async Task<IActionResult> RegisterAsync([FromBody] RegisterModel model)
+        {
+            if (!ModelState.IsValid)
+                return BadRequest(ModelState);
+
+            var result = await _authService.Register(model);
+
+            if (!result.IsAuthenticated)
+                return BadRequest(result.Message);
+
+            return Ok(result);
+        }
+
+
+        [HttpPost("login")]
+        public async Task<IActionResult> GetTokenAsync([FromBody] LoginRequest model)
+        {
+            if (!ModelState.IsValid)
+                return BadRequest(ModelState);
+
+            var result = await _authService.Login(model);
+
+            if (!result.IsAuthenticated)
+                return BadRequest(result.Message);
+
+            return Ok(result);
+        }
+    }
 }
